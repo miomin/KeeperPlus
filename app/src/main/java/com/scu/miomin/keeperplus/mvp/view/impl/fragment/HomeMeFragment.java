0ace7382354af3f2b1db5fec60b9c0b8466afe11
miomin <691292118@qq.com>
2016-12-06 19:39:36 +0800

    add some dialog

--- app/src/main/java/com/scu/miomin/keeperplus/mvp/view/impl/fragment/HomeMeFragment.java
+++ app/src/main/java/com/scu/miomin/keeperplus/mvp/view/impl/fragment/HomeMeFragment.java
@@ -15,6 +15,8 @@ import com.scu.miomin.keeperplus.mvp.view.impl.activity.SettingActivity;
 import com.scu.miomin.keeperplus.mvpcore.BaseToolbarMvpActivity;
 import com.scu.miomin.keeperplus.toolbar.ToolbarActivity;
 
+import me.drakeet.materialdialog.MaterialDialog;
+
 /**
  * Created by 莫绪旻 on 16/11/29.
  */
@@ -66,6 +68,42 @@ public class HomeMeFragment extends BaseFragment {
             @Override
             public void onClick(View v) {
                 SettingActivity.startActivity(getActivity());
+            }
+        });
+
+        fragmentView.findViewById(R.id.layout_bind_phone).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final MaterialDialog mMaterialDialog = new MaterialDialog(getActivity());
+                mMaterialDialog.setTitle("提示")
+                        .setMessage("您绑定的手机号码是" + tvPhonenumber.getText() + ",鉴于安全，暂不支持绑定后的手机号修改，请悉知！")
+                        .setPositiveButton("OK", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                mMaterialDialog.dismiss();
+                            }
+                        });
+
+                mMaterialDialog.show();
+            }
+        });
+
+        fragmentView.findViewById(R.id.layout_about_me).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final MaterialDialog mMaterialDialog = new MaterialDialog(getActivity());
+                mMaterialDialog.setTitle("关于开发者")
+                        .setMessage("开发者：莫绪旻，阿里巴巴手机淘宝团队，客户端开发工程师" + "\n"
+                                + "Github：https://github.com/miomin" + "\n"
+                                + "Email：miomin_93@foxmail.com" + "\n")
+                        .setPositiveButton("OK", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                mMaterialDialog.dismiss();
+                            }
+                        });
+
+                mMaterialDialog.show();
             }
         });
     }
